ab8059df3615b6ab4455d039c64faded88dd2076
Jonathan Ramos <jonathaniramos360@gmail.com>
2021-11-11 10:06:41 -0500

    created list of names

--- .github/lab 8.py
+++ .github/lab 8.py
@@ -1,6 +1,18 @@
-# Programmers: [your names]
+# Programmers: Jonathan, Alex, Sebastian
 # Course: CS151, Dr. Rajeev
-Date:
-Lab Number:
-Program Inputs: [What information do you request from the user?]
-Program Outputs:
+# Date: November 11, 2021
+# Lab Number: 8
+# Program Inputs: [What information do you request from the user?]
+# Program Outputs:
+
+def load_name_list(file_name):
+    names_file = open(file_name, "r")
+    list_of_names = []
+    for line in names_file:
+        list_of_names.append(line.strip())
+    names_file.close()
+    return list_of_names
+
+
+names = load_name_list("names.txt")
+print(names)
