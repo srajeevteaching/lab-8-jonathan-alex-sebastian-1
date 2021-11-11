02fcb492dded271263d378f1d94a1922986d97d0
Jonathan Ramos <jonathaniramos360@gmail.com>
2021-11-11 11:11:56 -0500

    Lab missing continuation feature

--- .github/lab 8.py
+++ .github/lab 8.py
@@ -4,6 +4,8 @@
 # Lab Number: 8
 # Program Inputs: [What information do you request from the user?]
 # Program Outputs:
+import random
+
 
 def load_name_list(file_name):
     names_file = open(file_name, "r")
@@ -14,5 +16,27 @@ def load_name_list(file_name):
     return list_of_names
 
 
-names = load_name_list("names.txt")
-print(names)
+def pop_random_name(list_of_names):
+    index_value = random.randrange(len(list_of_names))
+    name = list_of_names[index_value]
+    list_of_names.pop(index_value)
+    return name
+
+
+def get_name(user_file):
+    names = load_name_list(user_file)
+    print(names)
+    chosen_name = pop_random_name(names)
+    print(chosen_name)
+
+
+def main():
+    user_file = input("what is the name of your file: ")
+    try:
+        get_name(user_file)
+    except FileNotFoundError:
+        print("Sorry this file does not exist!")
+        exit()
+
+
+main()
